69f484c700220598b78ba39a7ae69660ee37c703
MiguelGonzales7 <miky.river@gmail.com>
2020-09-11 12:00:11 -0400

    Agregando codigo completo de la practica no1 de Fundamentos

---
+++ Practica1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////1.Declare 5 variables primitivas numéricas
+            //byte numeroByte;
+            //int numeroInt;
+            //long numeroLong;
+            //float numeroFloat;
+            //double numeroDouble;
+
+
+            ////2.Declare 3 variables primitivas de tipo texto
+            //char txtChar;
+            //string txtString;
+            //string txtNombre;
+
+
+            ////3.Declare 5 variables numéricas y asígneles un valor
+            //byte numByte=7;
+            //int numInt=50;
+            //long numLong=1234;
+            //float numFloat=70;
+            //double numDouble=7.777;
+
+
+            ////4.Declare 3 variables de texto e ingrese un valor por teclado.
+            //string valor1 = Console.ReadLine();
+            //string valor2 = Console.ReadLine();
+            //string valor3 = Console.ReadLine();
+
+
+            ////5.Ingrese valores numéricos por teclado y muéstrelos en consola con la frase “El número que ingreso es: ”
+            //int valorNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("El número que ingreso es: {0}", valorNum);
+
+
+            ////6.Realice 5 ejemplos de conversión implícita.
+            //int numint = 7;
+            //long numlong = numint;
+
+            //int numint2 = 9;
+            //decimal numdecimal = numint2;
+
+            //int numint1 = 7;
+            //byte numbyte = 77;
+            //numint1 = numbyte;
+
+            //float numfloat1 = 112;
+            //double numdouble = numfloat1;
+
+            //long numlong1 = 14;
+            //float numfloat = numlong1;
+
+
+            ////7.Realice 5 ejemplos de conversión explicita
+            //int numint = 777;
+            //byte numero = (byte)numint;
+
+            //string txtnum = "10";
+            //int numint2 = int.Parse(txtnum);
+
+            //string txtnum1 = "147";
+            //int numint3 = Convert.ToInt32(txtnum1);
+
+            //int numint4 = 74;
+            //byte numbyte = Convert.ToByte(numint4);
+
+            //long numlong = 6699;
+            //int numint5 = (int)numlong;
+
+
+            ////8.Ingrese dos números por teclado y verifique cual es el mayor y cual el menor
+            //Console.Write("Ingrese un numero:");
+            //string valor1 = Console.ReadLine();
+            //Console.Write("Ingrese un numero:");
+            //string valor2 = Console.ReadLine();
+            //int num1 = Convert.ToInt32(valor1);
+            //int num2 = Convert.ToInt32(valor2);
+            //if (num1 > num2)
+            //{
+            //    Console.WriteLine("El numero mayor es: {0} y el menor es: {1}", num1, num2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El numero mayor es: {0} y el menor es: {1}", num2, num1);
+            //}
+
+
+            ////9.Ingrese un numero por teclado y verifique si es par.
+            //Console.Write("Ingrese un numero: ");
+            //string valor1 = Console.ReadLine();
+            //int num1 = Convert.ToInt32(valor1);
+            //if (num1 % 2 == 0)
+            //    Console.WriteLine("El numero {0} es par", num1);
+            //else
+            //    Console.WriteLine("El numero {0} es impar", num1);
+
+
+
+            ////10.Ingrese un numero por teclado y verifique si es múltiplo de 3, 5 o 7
+            //Console.Write("Ingrese un numero: ");
+            //string valor1 = Console.ReadLine();
+            //int num1 = Convert.ToInt32(valor1);
+            //if (num1 % 3 == 0)
+            //    Console.WriteLine("el numero {0} es multiplo de 3", num1);
+            //else if (num1 % 5 == 0)
+            //    Console.WriteLine("el numero {0} es multiplo de 5", num1);
+            //else if (num1 % 7 == 0)
+            //    Console.WriteLine("el numero {0} es multiplo de 7", num1);
+            //else
+            //    Console.WriteLine("el numero {0} no es multiplo de 3, 5 o 7", num1);
+
+
+
+        }
+    }
+}
