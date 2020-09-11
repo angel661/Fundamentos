c940fb79e5550f913eef636200786d7fca149ae6
Edgarchc <edgar.chc@gmail.com>
2020-09-11 09:19:28 -0400

    2020-09-11 iniciando la subida al branch

---
+++ EJERCICIO 01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EJERCICIO 01");
+            Console.WriteLine("1 .- Declare 5 cinco variables primitivas numéricas");
+            Console.WriteLine("2 .- Declare 3 variables primitivas de tipo texto");
+            Console.WriteLine("3 .- Declare 5 variables numéricas y asígneles un valor");
+            Console.WriteLine("4.- Declare 3 variables de texto e ingrese un valor por teclado");
+            Console.WriteLine("5.- Ingrese valores numéricos por teclado y muéstrelos en consola con la frase “El número que ingreso es: ”");
+            Console.WriteLine("6.- Realice 5 ejemplos de conversión implícita");
+            Console.WriteLine("7.- Realice 5 ejemplos de conversión explicita");
+            Console.WriteLine("8.- Ingrese dos números por teclado y verifique cual es el mayor y cual el menor");
+            Console.WriteLine("9.- Ingrese un numero por teclado y verifique si es par");
+            Console.WriteLine("10.- Ingrese un numero por teclado y verifique si es múltiplo de 3, 5 o 7");
+        }
+    }
+}
