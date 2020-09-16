a498ddf555ce4ffc5a16d99617cc581965b4495d
EmilioAmaru <fausto2c2@gmail.com>
2020-09-16 19:52:39 -0400

    agregando codigo del primer ejercicio

---
+++ prueba practica01E1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace prueba_practica01E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int valor1; // declaramos la variable
+            //int valor2;
+            //valor1 = 2; // le asignamos un valor
+            //valor2 = 7;
+            //Console.WriteLine(valor1 + valor2);
+            //Console.ReadKey();
+
+            //float pi = 3.14159f;
+            //Console.WriteLine(pi);
+            //Console.ReadKey();
+
+            //double doble = 10.5;
+            //Console.WriteLine(doble);
+            //Console.ReadKey();
+
+            //char caracter = 'b';
+            //Console.WriteLine(caracter);
+            //Console.ReadKey();
+
+            string cadena = "amaru2020";
+            Console.WriteLine(cadena);
+            Console.ReadKey();
+        }
+    }
+}
