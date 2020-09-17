c73557f6ca1f2d70b5b6cca893113d365fb850c1
EmilioAmaru <fausto2c2@gmail.com>
2020-09-16 20:06:23 -0400

    agregando codigo ejercico2 de practica 01

---
+++ pract01ejercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pract01ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare 3 variables primitivas de tipo texto.
+
+            //1
+            //string nombre = "ramiro";
+            //Console.WriteLine(nombre);
+            //Console.ReadKey();
+
+            //2
+            //bool boleano = true;
+            //Console.WriteLine(boleano);
+            //Console.ReadKey();
+
+            //3
+
+            char letra;
+            letra = 'x';
+            Console.WriteLine("la letra es la siguiente: " + letra);
+            Console.ReadKey();
+
+        }
+    }
+}
