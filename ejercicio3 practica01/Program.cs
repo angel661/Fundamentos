fed0328d1bd8826dfbb32bc84a23ff74f0dae909
EmilioAmaru <fausto2c2@gmail.com>
2020-09-17 23:14:10 -0400

    agregando ejercicio3 de practica 01

---
+++ ejercicio3 practica01/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ejercicio3_practica01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.Declare 5 variables numéricas y asígneles un valor.
+            //1
+            //int entero = -4;
+            //Console.WriteLine(entero);
+            //Console.WriteLine("el numero entero es: {0}", entero);
+            //Console.ReadLine();
+            //Console.ReadKey();
+
+
+            //2
+            //uint numero = 36;
+            //uint numero2 = 6;
+            //uint resultado = numero / numero2;
+            //Console.WriteLine("El rsultado de la división es {0}", resultado);
+            //Console.ReadLine();
+            //Console.ReadKey();
+
+            //3
+            //int numero = 25;
+            //float numero2 = 5.5f;
+            //float resultado = numero / numero2;
+            //Console.WriteLine("El resultado de la división es {0}", resultado);
+            //Console.ReadLine();
+            //Console.ReadKey();
+
+            //4
+
+            //char letra;
+            //Console.WriteLine("Por favor, ingrese una letra");
+            //letra = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(" la letra que ingreso es {0}", letra);
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //5
+            //Console.Write("Por favor, ingrese su nombre: ");
+            //string respuesta = Console.ReadLine();
+            //string nombre = "amaru";
+            //if (respuesta == nombre)
+            //    Console.WriteLine("Bienvenido! amaru ");
+            //else
+            //    Console.WriteLine("No estas en la lista ...");
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //6
+            int numero1 = 2;
+            int numero2 = 8;
+            int suma = 0;
+            suma = numero1 + numero2;
+           
+                if (suma >= 10)
+                {
+                    Console.WriteLine("verdad");
+                }
+                else
+                {
+                    Console.WriteLine("falso");
+                }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+    }
+}
