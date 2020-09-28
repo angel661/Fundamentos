d04c9c264b9d244338b0227483a5cab7244cb90e
marco20figue <mark.figue2014@gmail.com>
2020-09-27 21:49:47 -0400

    Agregando la resolucion de la Practica 1 ejercicios del 1 al 10 de fundamentos

---
+++ Practica1/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de 5 varibles primitivas nuemricas y asignacion de un valor;
+            //int entero;
+            //    entero = 3;
+            //Console.WriteLine(entero);
+
+            //long enteroGrande;
+            //enteroGrande = 2456347890;
+            //Console.WriteLine(enteroGrande);
+
+            //float numeroDecimal;
+            //numeroDecimal= 10.2365f;
+            //Console.WriteLine(numeroDecimal);
+
+            //double numeroDecimalGrande;
+            //numeroDecimalGrande = 214.234567190;
+            //Console.WriteLine(numeroDecimalGrande);
+
+            //decimal numeroDecimalMonetario;
+            //numeroDecimalMonetario = 345.234578m;
+            //Console.WriteLine(numeroDecimalMonetario);
+
+            // Declaracion de 3 variables primitivas de tipo texto e ingrese un valor por teclado
+            //bool esEstudiante = true;
+            //Console.WriteLine(esEstudiante);
+
+            //string texto1 = "Hola esta es la presentacion de la practica 1";
+            //Console.WriteLine(texto1);
+
+            //char caracter1 = 'y';
+            //Console.WriteLine(caracter1);
+
+            //StringBuilder texto2 = new StringBuilder();
+            //texto2.Append("La presentacion de la practica 2 ");
+            //texto2.Append(" y la presentacion de la practica 3");
+            //Console.WriteLine(texto2);
+            //Cosole.ReadLine();
+
+            //Ingresando valor numerico por teclado y mostrando en consola con la frase "El numero que ingreso es:"
+
+            // Variables
+            //string dato = "";
+            //int valor = 0;
+
+            //// Pedir un numero
+            //Console.WriteLine("Escriba un numero");
+            //dato = Console.ReadLine();
+            //valor = Convert.ToInt32(dato);
+
+            //// Mostrar
+            //Console.WriteLine("El nuemro que Ingreso es: {0}", valor);
+
+            // Ejemplos de conversion implicita
+
+            //byte numero1 = 150;
+            //short numeroMadiano1 = numero1;
+            //int numeroMediano2 = numeroMadiano1;
+            //long numeroGrande = numeroMediano2;
+            //double numeroReal = numeroGrande;
+
+            //Console.WriteLine(numero1);
+
+            //int a = 255;
+            //byte b= 254;
+            //a = b;
+            //Console.WriteLine(a);
+
+            //Ejemplos de converson Explicicta
+
+            //int grande = 255;
+            //byte pequeño = 254;
+            //pequeño = (byte) grande;
+            //Console.WriteLine(grande);
+
+            //string texto = "258";
+            //int numero = Convert.ToInt32(texto);
+            //Console.WriteLine(texto);
+
+            //string texto = "456,464646";
+            //double numDecimal = Convert.ToDouble(texto);
+            //Console.WriteLine(texto);
+
+            // Ingrese 2 numeros por teclado y verificque cual es el mayor y cual es menor
+
+            //int numero1, numero2;
+            //Console.WriteLine("Ingrese el Primer Numero: ");
+            //numero1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Ingrese el Segundo Numero: ");
+            //numero2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (numero1 > numero2)
+            //{
+            //    Console.WriteLine("El numero mayor es; {0}", numero1);
+            //    Console.WriteLine("Y el numero menor es: {0}", numero2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El numero menor es: {0}", numero2);
+            //}
+
+            //Ingrese un numero por teclado y verifique si es par o impar
+
+            //int numero1 = 0;
+            //Console.WriteLine("Ingrese un numero: ");
+            //numero1 = Convert.ToInt32(Console.ReadLine());
+            //if (numero1 % 2 == 0)
+            //{
+            //    Console.WriteLine("El numero es par ");
+            //}
+            //else {
+            //    Console.WriteLine("El numero es impar");
+            ////}
+            //Ingrese un numero por el teclado y verifique si es multiplo de 3 5 o 7
+
+            //int num1;
+            //int num2 = 3;
+            //int num3 = 5;
+            //int num4 = 7;
+
+            //Console.WriteLine("Ingrese un numero: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //if (num1 % num2 == 0)
+            //{
+            //    Console.WriteLine("El numero: {0}, es multiplo de: {1}", num1, num2);
+            //}
+            //    if (num1 % num3 == 0)
+            //    {
+            //        Console.WriteLine("El numero: {0}, es multiplo de: {1}", num1, num3);
+            //    }
+            // if (num1 % num4 == 0)
+            //{
+            //    Console.WriteLine("El numero: {0}, es multiplo de: {1}", num1, num4);
+            //}
+            //Console.ReadKey();
+            //Console.ReadLine();
+        }
+    }
+}
