2d194b5cc771746d4d5d392082dedab43181420a
EmilioAmaru <fausto2c2@gmail.com>
2020-10-07 23:30:39 -0400

    agregando ejercicio 11-12-13-14-16 practica03

--- practica03ejerc01/Program.cs
+++ practica03ejerc01/Program.cs
@@ -156,34 +156,169 @@ namespace practica03ejerc01
             //10
             //suma de los números impares y la cantidad
 
-            int numero;
-            int par = 0;
-            int impar = 0;
-            int sumapar = 0;
-            int sumaimpar = 0;
-            int contador = 0;
+            //int numero;
+            //int par = 0;
+            //int impar = 0;
+            //int sumapar = 0;
+            //int sumaimpar = 0;
+            //int contador = 0;
 
-            Console.Write("por favor, ingrese un numero:  ");
-            numero = Convert.ToInt32(Console.ReadLine());
-            for (int x = 1; x <= numero; x++)
-            {
-                int y = x % 2;
-                if (y != 0)
-                {
-                    contador = contador + 1;
-                    Console.Write("{0}|", x);
-                    sumapar += x;
-                    impar++;
-                }
+            //Console.Write("por favor, ingrese un numero:  ");
+            //numero = Convert.ToInt32(Console.ReadLine());
+            //for (int x = 1; x <= numero; x++)
+            //{
+            //    int y = x % 2;
+            //    if (y != 0)
+            //    {
+            //        contador = contador + 1;
+            //        Console.Write("{0}|", x);
+            //        sumapar += x;
+            //        impar++;
+            //    }
 
-            }
-            Console.WriteLine("\n\n");
-            Console.WriteLine("la suma de numeros impares es:" + sumapar);
-            Console.WriteLine("total de numeros imapar es:" + contador);
-            Console.ReadLine();
+            //}
+            //Console.WriteLine("\n\n");
+            //Console.WriteLine("la suma de numeros impares es:" + sumapar);
+            //Console.WriteLine("total de numeros imapar es:" + contador);
+            //Console.ReadLine();
 
 
+            //11
+            // ordenar los numeros forma creciente
+            //Console.WriteLine("ingrese tres numeros: ");
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+            //int mayor;
+            //int menor;
+            //int segundoNumero;
+
+            //if (a < b && a < c)
+            //{
+            //    mayor = a;
+            //}
+            //else if (b < a && b < c)
+            //{
+            //    mayor = b;
+            //}
+            //else
+            //{
+            //    mayor = c;
+            //}
+
+            //if (a > b && a > c)
+            //{
+            //    menor = a;
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    menor = b;
+            //}
+            //else
+            //{
+            //    menor = c;
+            //}
+
+            //if (a < menor && a > mayor)
+            //{
+            //    segundoNumero = a;
+            //}
+            //else if (b < menor && b > mayor)
+            //{
+            //    segundoNumero = b;
+            //}
+            //else
+            //{
+            //    segundoNumero = c;
+            //}
+
+            //Console.WriteLine("Los valores ordenados de menor a mayor es: {0} {1} {2}", mayor, segundoNumero, menor);
+            //Console.WriteLine();
+            //Console.ReadLine();
+
+            //12
+
+            // intervalo entre dos numeros
+            //Console.Write("Ingrese el limite inferior: \t ");
+            //int numeroInferior = int.Parse(Console.ReadLine());
+            //Console.Write("Ingrese el limite superior: \t");
+            //int numeroSuperior = int.Parse(Console.ReadLine());
+            //Console.Clear();
+
+            //for (int i = numeroInferior; i <= numeroSuperior; i += 2)
+            //{
+            //    Console.Write(i);
+            //    Console.WriteLine(" ");
+
+            //}
+            //Console.ReadKey();
+
+            //13
+
+            // intervalo entre dos numeros impares
+            //int cont = 0, contimpar = 0;
+            //Console.Write("Ingrese el limite inferior: \t ");
+            //int numeroInferior = int.Parse(Console.ReadLine());
+            //Console.Write("Ingrese el limite superior: \t");
+            //int numeroSuperior = int.Parse(Console.ReadLine());
+            //Console.Clear();
+
+            //for (int i = numeroInferior + 1; i < numeroSuperior; i += 2)
+            //{
+            //    Console.Write("{0}|", i);
+            //    cont++;
+            //    if (i % 2 != 0)
+            //        contimpar++;
+            //}
+
+            //Console.ReadKey();
+
+            //14 Que pida dos números y sume todos los números 
+            //int num1 = 0, num2 = 0;
+            //double suma = 0;
+
+            //Console.Write("Ingrese el primer numero:  ");
+            //int de = int.Parse(Console.ReadLine());
+            //Console.Write("Ingrese el segundo numero:  ");
+            //int ha = int.Parse(Console.ReadLine());
+
+            //if (de > ha)
+            //{
+            //    num1 = de;  
+            //    num2 = ha;
+            //    de = num2;
+            //    ha = num1;
+            //}
+
+
+            //if ((ha - de >= 1) && (ha > 0) && (de > 0))
+            //{
+
+            //    for (int i = de; i <= ha; i++)
+            //    {
+
+            //        suma = suma + (Math.Pow(i,1));
+
+            //    }
+
+            //    Console.WriteLine("Sumatoria de todo es: " + suma);
+            //}
+            //else { Console.WriteLine("Deben ser numeros mayores a cero, el rango de A hasta B debe de ser mayor 1"); }
+
+            //Console.ReadKey();
+
+            //15 Que pida dos números y multiplique todos los números 
+
+            //16 Que pida un número y muestre en pantalla el mismo número de asteriscos.
+            int i, num;
+            Console.Write("Ingrese un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < num; i++)
+            Console.Write(" * ");
+            Console.ReadKey();
 
         }
     }
+    
 }
