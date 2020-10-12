89a7bab5c00cc749714c345abb487e7331689d66
AlbertoSoria <betoxd2003@gmail.com>
2020-10-11 20:55:22 -0400

    Agregando prácticas 2 (condicionales) y 3 (ciclos)

---
+++ Practica 3 (ciclos)/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practica_3__ciclos_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------1
+            Console.WriteLine("Introduzca un numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            bool primo = true;
+            for(int i = 2; i * i <= numero; i++)
+            {
+                if (numero % 2 == 0) primo = false;
+            }
+            if (primo && numero != 0 && numero != 1 ) Console.WriteLine("Es primo");
+            else Console.WriteLine("No es primo");
+            //-----------------------------------------------2
+            for(int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+            //-----------------------------------------------3
+            for(int i = 100; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            //-----------------------------------------------4
+            for(int index = 1; index < 101; index++)
+            {
+                if(index % 2 == 0)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+            //-----------------------------------------------5
+            for (int index = 1; index < 101; index++)
+            {
+                if (index % 2 != 0)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+            //-----------------------------------------------6
+            int suma = 0, sumar = (100*101)/2;
+            for(int i = 1; i <= 100; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("{0}, {1}", suma, sumar);
+            //-----------------------------------------------7
+            suma = 0;
+            for(int i = 2; i <= 100; i = i + 2)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine(suma);
+            //-----------------------------------------------8
+            suma = 0;
+            for(int i = 1; i <= 100; i = i + 2)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine(suma);
+            //-----------------------------------------------9
+            suma = 0;
+            int numeroDePares = 0;
+            for(int i = 2; i <= 100; i = i + 2)
+            {
+                suma = suma + i;
+                numeroDePares++;
+            }
+            Console.WriteLine("La suma es: {0} y hay {1} numeros pares", suma, numeroDePares);
+            //-----------------------------------------------10
+            suma = 0;
+            int numeroDeImpares = 0;
+            for (int i = 1; i <= 100; i = i + 2)
+            {
+                suma = suma + i;
+                numeroDeImpares++;
+            }
+            Console.WriteLine("La suma es: {0} y hay {1} numeros pares", suma, numeroDeImpares);
+            //-----------------------------------------------11
+            Console.WriteLine("Introduzca dos numeros");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            int aux;
+            if (numero1 <= numero2)
+            {
+                for (int i = numero1; i <= numero2; i++)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+            else
+            {
+                for (int i = numero1; i >= numero2; i--)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+            //-----------------------------------------------12
+            Console.WriteLine("Introduzca dos numeros");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            if (numero1 <= numero2)
+            {
+                for (int i = numero1; i <= numero2; i++)
+                {
+                    if (i % 2 == 0) Console.WriteLine("{0} ", i);
+                }
+            }
+            else
+            {
+                for (int i = numero1; i >= numero2; i--)
+                {
+                    if (i % 2 == 0) Console.WriteLine("{0} ", i);
+                }
+            }
+            //-----------------------------------------------13
+            Console.WriteLine("Introduzca dos numeros");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            if (numero1 <= numero2)
+            {
+                for (int i = numero1; i <= numero2; i++)
+                {
+                    if (i % 2 != 0) Console.WriteLine("{0} ", i);
+                }
+            }
+            else
+            {
+                for (int i = numero1; i >= numero2; i--)
+                {
+                    if (i % 2 != 0) Console.WriteLine("{0} ", i);
+                }
+            }
+            //-----------------------------------------------14
+            Console.WriteLine("Introduzca dos numeros");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            aux = Math.Max(numero1, numero2);
+            numero1 = Math.Min(numero1, numero2);
+            numero2 = aux;
+            suma = 0;
+            for (int i = numero1; i <= numero2; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine(suma);
+            //-----------------------------------------------15
+            Console.WriteLine("Introduzca dos numeros");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            aux = Math.Max(numero1, numero2);
+            numero1 = Math.Min(numero1, numero2);
+            numero2 = aux;
+            long multiplicar = 1;
+            for (int i = numero1; i <= numero2; i++)
+            {
+                multiplicar = multiplicar * i;
+            }
+            Console.WriteLine(multiplicar);
+            //-----------------------------------------------16
+            Console.WriteLine("Introduzca un numero");
+            int numeroDeAsteriscos = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < numeroDeAsteriscos; i++)
+            {
+                Console.WriteLine("*");
+            }
+            //-----------------------------------------------17
+            aux = 1;
+            for(int i = 0; i < 10; i++)
+            {
+                for(int l = 0; l < 10; l++)
+                {
+                    Console.Write("{0} ",aux);
+                    if (aux < 10) Console.Write(" ");
+                    aux++;
+                }
+                Console.WriteLine();
+            }
+            //-----------------------------------------------18
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.Write("La tabla del {0}: ", i);
+                for(int l = 1; l <= 10; l++)
+                {
+                    Console.Write("{0} ",l * i);
+                }
+                Console.WriteLine();
+            }
+            //-----------------------------------------------19
+            numero = 20;
+            Console.Write("La tabla de multiplicar del numero {0} es: ",numero);
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write("{0} ", i * numero);
+            }
+            Console.WriteLine();
+            //-----------------------------------------------20
+            while (true)
+            {
+                Console.WriteLine("Desea Salir (S/N)");
+                char entrada = Convert.ToChar(Console.ReadLine());
+                if (entrada == 'S') break;
+            }
+            //-----------------------------------------------21
+            long numeroG = 15;
+            long limite = numeroG;
+            numeroG = 1;
+            for(long i = 1; i < limite; i++)
+            {
+                numeroG *= i;
+            }
+            Console.WriteLine(numeroG);
+            //-----------------------------------------------22
+            int[] numeros = { 10, 12, 214, 12421, 124, 12, 5463, 356, 232, 342};
+            aux = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                aux += numeros[i];
+            }
+            double media = aux / 10;
+            Console.WriteLine(media);
+            //-----------------------------------------------23
+            aux = 0; int aux1 = 0;
+            numero = Convert.ToInt32(Console.ReadLine());
+            while(numero != 0)
+            {
+                aux = aux + numero;
+                aux1++;
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+            if (aux1 > 0)
+            {
+                media = aux / aux1;
+            }
+            else
+            {
+                media = 0;
+            }
+            Console.WriteLine(media);
+            //-----------------------------------------------24
+            suma = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                suma = suma + (i * i);
+            }
+            Console.WriteLine(suma);
+            //-----------------------------------------------25
+            numero1 = 1;
+            numero2 = 1;
+            for(long i = 0; i < 25; i++)
+            {
+                Console.Write("{0} ", numero2);
+                aux = numero1 + numero2;
+                numero2 = numero1;
+                numero1 = aux;
+            }
+        }
+    }
+}
---
+++ Práctica 2 (Condicionales)/Program.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Text;
+namespace Práctica_2__Condicionales_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------1
+            int edad = Convert.ToInt32(Console.ReadLine());
+            if (edad >= 18) Console.WriteLine("Es mayor de edad");
+            else Console.WriteLine("No es mayor de edad");
+            //-----------------------------------------------2
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 25) Console.WriteLine("Es mayor que 25");
+            else Console.WriteLine("No es mayor que 25");
+            //-----------------------------------------------3
+            string nombre1, nombre2, apellido1="", apellido2="", aux1="", aux2="";
+            Console.WriteLine("Introduzca nombres y apellidos de dos personas");
+            Console.Write("Introduzca el nombre de la primera persona: "); nombre1 = Console.ReadLine();
+            Console.Write("Introduzca los apellidos de la primera persona: "); apellido1 = Console.ReadLine();
+            Console.Write("Introduzca el nombre de la segunda persona: "); nombre2 = Console.ReadLine();
+            Console.Write("Introduzca los apellidos de la segunda persona: "); apellido2 = Console.ReadLine();
+            nombre2 = Console.ReadLine();
+            aux1 = apellido1 + " " + nombre1;
+            aux2 = apellido2 + " " + nombre2;
+            if (String.Compare(aux1,aux2)==-1)
+            {
+                Console.WriteLine(aux1);
+                Console.WriteLine(aux2);
+            }
+            else
+            {
+                Console.WriteLine(aux2);
+                Console.WriteLine(aux1);
+            }
+            //-----------------------------------------------4
+            string status ="no";
+            Console.WriteLine("Ingrese su edad:"); edad = Convert.ToInt32(Console.ReadLine());
+            if (edad < 21)
+            {
+                Console.WriteLine("¿Son socios sus padres?:"); status = Console.ReadLine();
+            }
+            double nueva_cuota = 800;
+            if(edad > 65)
+            {
+                nueva_cuota = nueva_cuota - (800 * .4);
+            }
+            else if(edad < 21 && status == "No")
+            {
+                nueva_cuota = nueva_cuota - (800 * .25);
+            }else if(edad <21 && status == "Si")
+            {
+                nueva_cuota = nueva_cuota - (800 * .45);
+            }
+            Console.WriteLine(nueva_cuota);
+            //-----------------------------------------------5
+            int dia, mes, año;
+            bool fecha_valida = false;
+            dia = Convert.ToInt32(Console.ReadLine());
+            mes = Convert.ToInt32(Console.ReadLine());
+            año = Convert.ToInt32(Console.ReadLine());
+            if(año % 4 == 0)
+            {
+                if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && dia < 32) fecha_valida = true;
+                else if (mes == 2 && dia < 30) fecha_valida = true;
+                else if (mes < 13 && dia < 31) fecha_valida = true;
+            }
+            else
+            {
+                if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && dia < 32) fecha_valida = true;
+                else if (mes == 2 && dia < 29) fecha_valida = true;
+                else if (mes < 13 && dia < 31) fecha_valida = true;
+            }
+            if (fecha_valida) Console.WriteLine("Fecha correcta");
+            else Console.WriteLine("Fecha Incorrecta");
+            //-----------------------------------------------6
+            Console.WriteLine("Por favor introduzca una nota entre 0 y 10");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            if (nota < 5) Console.WriteLine("Reprueba");
+            else if (nota < 7) Console.WriteLine("Aprobado");
+            else if (nota < 9) Console.WriteLine("Notable");
+            else if (nota < 10) Console.WriteLine("Sobresaliente");
+            else if (nota == 10) Console.WriteLine("Excelente");
+            //-----------------------------------------------7
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Log10(numero));
+            //-----------------------------------------------8
+            numero = Convert.ToInt32(Console.ReadLine());
+            int suma = 0;
+            while(numero > 0)
+            {
+                suma = suma + (numero % 10);
+                numero = numero / 10;
+            }
+            Console.WriteLine(suma);
+            //-----------------------------------------------9
+            int A = 5, B = 3, C = -12;
+            if (A > 3) Console.WriteLine("Se cumple A > 3"); else Console.WriteLine("No se cumple A > 3");
+            if (A > C) Console.WriteLine("Se cumple A > C"); else Console.WriteLine("No se cumple A > C");
+            if (B != C) Console.WriteLine("Se cumple B != C"); else Console.WriteLine("No se cumple B != c");
+            if (A == 3) Console.WriteLine("Se cumple A == 3"); else Console.WriteLine("No se cumple A == 3");
+            if (A * B == 15) Console.WriteLine("Se cumple A * B == 15"); else Console.WriteLine("No se cumple A * B == 15");
+            if (C / B < A) Console.WriteLine("Se cumple C / B < A"); else Console.WriteLine("No se cumple C / B < A");
+            if (C / B == -10) Console.WriteLine("Se cumple C / B == -10"); else Console.WriteLine("No se cumple C / B == -10");
+            if (A + B + C == 5) Console.WriteLine("Se cumple A + B + C == 5"); else Console.WriteLine("No se cumple A + B + C == 5");
+            if ((A + B == 8) && (A - B == 2)) Console.WriteLine("Se cumple (A+B == 8) && (A-B == 2) "); else Console.WriteLine("No se cumple (A+B == 8) && (A-B == 2) ");
+            if ((A + B == 8) || (A - B == 6)) Console.WriteLine("Se cumple (A+B == 8) || (A-B == 6)"); else Console.WriteLine("No se cumple (A+B == 8) || (A-B == 6)");
+            if (A > 3 && B > 3 && C < 3) Console.WriteLine("Se cumple A > 3 && B > 3 && C < 3"); else Console.WriteLine("No se cumple A > 3 && B > 3 && C < 3");
+            //-----------------------------------------------10
+            Console.WriteLine("Introduzca un número del 1 al 5");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if(numero == 2 || numero == 3 || numero == 5)
+            {
+                Console.WriteLine("Es un numero primo");
+            }
+            else
+            {
+                Console.WriteLine("No es un numero primo");
+            }
+            //-----------------------------------------------11
+            Console.WriteLine("Introduzca un número del 1 al 7");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero == 1) Console.WriteLine("Lunes");
+            else if (numero == 2) Console.WriteLine("Martes");
+            else if (numero == 3) Console.WriteLine("Miercoles");
+            else if (numero == 4) Console.WriteLine("Jueves");
+            else if (numero == 5) Console.WriteLine("Viernes");
+            else if (numero == 6) Console.WriteLine("Sabado");
+            else if (numero == 7) Console.WriteLine("Domingo");
+            //-----------------------------------------------12
+            Console.WriteLine("Introduzca un número del 1 al 12");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero == 1) Console.WriteLine("Enero");
+            else if (numero == 2) Console.WriteLine("Febrero");
+            else if (numero == 3) Console.WriteLine("Marzo");
+            else if (numero == 4) Console.WriteLine("Abril");
+            else if (numero == 5) Console.WriteLine("Mayo");
+            else if (numero == 6) Console.WriteLine("Junio");
+            else if (numero == 7) Console.WriteLine("Julio");
+            else if (numero == 3) Console.WriteLine("Agosto");
+            else if (numero == 4) Console.WriteLine("Septiembre");
+            else if (numero == 5) Console.WriteLine("Octubre");
+            else if (numero == 6) Console.WriteLine("Noviembre");
+            else if (numero == 7) Console.WriteLine("Diciembre");
+            //-----------------------------------------------13
+            Console.WriteLine("Introduzca 3 numeros");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            int numero3 = Convert.ToInt32(Console.ReadLine());
+            int min;
+            min = Math.Min(numero1, Math.Min(numero2, numero3));
+            if(numero1 == min && numero2 == min && numero3 == min)
+            {
+                Console.WriteLine("{0}, {1}, {2}", numero1, numero2, numero3);
+            }
+            else if (numero1 == min && numero2 == min)
+            {
+                Console.WriteLine("{0}, {1}", numero1, numero2);
+                Console.WriteLine("{0}", numero3);
+            }
+            else if (numero2 == min && numero3 == min)
+            {
+                Console.WriteLine("{0}, {1}", numero2, numero3);
+                Console.WriteLine("{0}", numero1);
+            }
+            else if (numero1 == min && numero3 == min)
+            {
+                Console.WriteLine("{0}, {1}", numero1, numero3);
+                Console.WriteLine("{0}", numero2);
+            }
+            else if (numero1 == min)
+            {
+                Console.WriteLine("{0}", numero1);
+                Console.WriteLine("{0}", Math.Min(numero2, numero3));
+                Console.WriteLine("{0}", Math.Max(numero2, numero3));
+            }
+            else if (numero2 == min)
+            {
+                Console.WriteLine("{0}", numero2);
+                Console.WriteLine("{0}", Math.Min(numero1, numero3));
+                Console.WriteLine("{0}", Math.Max(numero1, numero3));
+            }
+            else if (numero3 == min)
+            {
+                Console.WriteLine("{0}", numero3);
+                Console.WriteLine("{0}", Math.Min(numero2, numero1));
+                Console.WriteLine("{0}", Math.Max(numero2, numero1));
+            }
+            //-----------------------------------------------14
+            Console.WriteLine("Introduzca un número");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero >= 0) Console.WriteLine("El número es postivio");
+            else Console.WriteLine("El numero es negativo");
+            //-----------------------------------------------15
+            Console.WriteLine("Introduzca 10 numeros");
+            int max = -100000000 ;min = 10000000;
+            for(int i = 0; i < 10; i++)
+            {
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (i == 0)
+                {
+                    max = numero;
+                    min = numero;
+                }
+                else
+                {
+                    max = Math.Max(numero, max);
+                    min = Math.Max(numero, min);
+                }
+            }
+            Console.WriteLine("El numero mayor es: {0} y el numero menor es: {1}", max, min);
+            //-----------------------------------------------16
+            Console.WriteLine("Introduzca tres numeros");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            numero3 = Convert.ToInt32(Console.ReadLine());
+            if (numero3 == numero1 + numero2) Console.WriteLine("El tercer numero es igual a la suma del primer numero mas el segundo");
+            else Console.WriteLine("El tercer numero no es igual a la suma del primer numero mas el segundo");
+            //-----------------------------------------------17
+            while (true)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("->Archivo");
+                Console.WriteLine("->Buscar");
+                Console.WriteLine("->Salir");
+                string menu_elegido = Console.ReadLine();
+                if(menu_elegido == "Arichivo" || menu_elegido == "Buscar" || menu_elegido == "Salir")
+                {
+                    Console.WriteLine("El menu elegido es correcto, puede avanzar");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("El menu elegido es incorrecto, vuele a intentarlo");
+                }
+            }
+            //-----------------------------------------------18
+            Console.WriteLine("Introduzca un valor en metros para convertir a otra unidad mostrada abajo");
+            Console.WriteLine("->Pulgadas");
+            Console.WriteLine("->Centimentros");
+            Console.WriteLine("->Kilometros");
+            Console.WriteLine("->Millas");
+            Console.WriteLine("->Millas nauticas");
+            Console.WriteLine("->Pies");
+            string medida = Console.ReadLine();
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (medida == "Pulgadas") Console.WriteLine(numero * 39.37);
+            else if (medida == "Centimetros") Console.WriteLine(numero * 100);
+            else if (medida == "Kilometros") Console.WriteLine(numero * 0.001);
+            else if (medida == "Millas") Console.WriteLine(numero * 0.000621);
+            else if (medida == "Millas nauticas") Console.WriteLine(numero * 0.0005);
+            else if (medida == "Pies") Console.WriteLine(numero * 3.28084);
+            //-----------------------------------------------19
+            double importe, impuesto;
+            importe = Convert.ToInt32(Console.ReadLine());
+            if (importe > 15000) impuesto = importe * 0.16;
+            else impuesto = importe * 0.1;
+            importe = importe -impuesto;
+            //-----------------------------------------------20
+            int añosDeAntiguedad = Convert.ToInt32(Console.ReadLine());
+            double nuevoSueldo = 40000;
+            if(añosDeAntiguedad < 3)
+            {
+                nuevoSueldo += nuevoSueldo * 0.03;
+            }
+            else if(añosDeAntiguedad < 5)
+            {
+                nuevoSueldo += nuevoSueldo * 0.05;
+            }
+            else if(añosDeAntiguedad < 10)
+            {
+                nuevoSueldo += nuevoSueldo * 0.07;
+            }
+            else if(añosDeAntiguedad >= 10)
+            {
+                nuevoSueldo += nuevoSueldo * 0.1;
+            }
+            Console.WriteLine("El nuevo sueldo es {0}", nuevoSueldo);
+        }
+    }
+}
